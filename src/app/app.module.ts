f26fce3b68e35ce04f39e4fd3c89d4dd133ca469
Ossman <carvajalossman@gmail.com>
2021-04-22 20:06:40 -0500

    Hecha la maquetación del componente de uploading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UploadComponent } from './upload/upload.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { UploadingComponent } from './components/uploading/uploading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UploadComponent
+    UploadComponent,
+    UploadingComponent
   ],
   imports: [
     BrowserModule,
